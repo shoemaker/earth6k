b45de79074c0f3db3d6d5ca3b06ec2e4b2569e21
Brian Shoemaker <brianshoemaker@gmail.com>
2015-05-24 19:04:43 -0500

    Refactored to pull sources from file.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
+var fs = require('fs');
 
 /*
  * GET home page.
  */
+exports.index = function(req, res) {
+    var model = {
+        year : new Date().getFullYear(),
+        sources : null
+    };
 
-exports.index = function(req, res){
-  res.render('index', { year: new Date().getFullYear() });
+    // Load sources for more reading
+    fs.readFile(process.cwd() + '/sources.json', function(err, sources) {
+        sources = JSON.parse(sources);
+        model.sources = sources;
+        res.render('index', model);  
+    });  
 };
